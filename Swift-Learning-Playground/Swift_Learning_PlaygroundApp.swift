1df29eabf1cfe29b4c36a1c6d7e504c4eedcfbfb
Raul Gallego <raulgallego79@icloud.com>
2025-01-02 14:02:43 +0100

    Initial Commit

---
+++ Swift-Learning-Playground/Swift_Learning_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Learning_PlaygroundApp.swift
+//  Swift-Learning-Playground
+//
+//  Created by Raúl Gallego Alonso on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Learning_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
